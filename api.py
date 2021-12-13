925592a132263341137302000c7f543439ecd274
BlingCc <54300418+BlingCc233@users.noreply.github.com>
2021-12-13 14:11:53 +0800

    Add files via upload

--- api.py
+++ api.py
@@ -8,21 +8,23 @@ import random
 status = 0
 
 def keyword_pr(message, uid):
-    if message[0:1] == '火': 
-        xuhuo(uid)
-    if message[0:2] == '复读':
-        fudu_pr(message,uid)
-    if message[0:1] == '在':
-        hello(uid)
-    if message == '/help':
-        help(uid,None)
-    if message[0:9] == '/feedback':
-        feedback(message,uid,None)
-    if message[0:7] == '/status':
-        set_status(message,uid)
+    if status == 0:
+        if message[0:1] == '火': 
+            xuhuo(uid)
+        if message[0:2] == '复读':
+            fudu_pr(message,uid)
+        if message[0:1] == '在':
+            hello(uid)
+        if message == '/help':
+            help(uid,None)
+        if message[0:9] == '/feedback':
+            feedback(message,uid,None)
+        if message[0:7] == '/status':
+            set_status(message,uid)
     if status == 1:
-        if uid != 0:
-            ignore(uid)
+        if message[0:7] == '/status':
+            set_status(message,uid)
+        ignore(uid)
      
 #违禁词
 bans = ['妈卖批','科学上网', '傻逼' , '艹', '卧槽' ,'woc']
@@ -42,22 +44,28 @@ def keyword_gr(message, drawback_msg, uid, gid):
                 drawback(drawback_msg,uid,gid)
 
 
-
+#上报地址
 uurl = 'http://127.0.0.1:22333'
 
 #功能
+#自动续火
 def xuhuo(uid):
 	requests.get(url=uurl+'/send_private_msg?user_id={0}&message=火'.format(uid))
 
+#复读机
 def fudu_gr(message,uid,gid):
     requests.get(url=uurl+'/send_group_msg?group_id={0}&message={1}'.format(gid,r'[CQ:at,' r'qq=' + str(uid) + r']' + message[2: ]))
 
+#复读机，因为优化问题写了两个，不改了
 def fudu_pr(message,uid):
     requests.get(url=uurl+'/send_private_msg?user_id={0}&message={1}'.format(uid,message[2: ]))
 
+#去他妈的“在”
 def hello(uid):
-    requests.get(url=uurl+'/send_private_msg?user_id={0}&message={1}'.format(uid,r'[CQ:face,id=181]'))
-    
+    for 1 in 10 :
+        requests.get(url=uurl+'/send_private_msg?user_id={0}&message={1}'.format(uid,r'[CQ:face,id=181]'))
+
+#菜单？    
 def help(uid,gid):
     msg = '我是Cc的机器人，暂时冇有任何功能：\n1.私聊回复" 在 "可以续火\n2.在特定群可以当管理并撤回违禁词。\n3.回复" 复读XXX "可以复读'
     if gid != None:
@@ -65,25 +73,28 @@ def help(uid,gid):
     else:
         requests.get(url=uurl+'/send_private_msg?user_id={0}&message={1}'.format(uid,msg))
     
+#群聊撤回    
 def drawback(msg_id,uid,gid):
     msg = ['富强','民主','文明','和谐','自由','平等','公正','法治','爱国','敬业','诚信','友善']
     rand = random.randint(0,11)
     requests.get(url=uurl+'/delete_msg?message_id={0}'.format(msg_id))
     requests.get(url=uurl+'/send_group_msg?group_id={0}&message={1}'.format(gid,r'[CQ:at,' r'qq=' + str(uid) + r']' + '已撤回违禁词。\n学习社会主义核心价值观：' + msg[rand]))
-    
+ 
+#状态设置       
 def set_status(message,uid):
     getnum = message[8:9]
     statu = int(getnum)
     global status
-    status = statu
-    requests.get(url=uurl+'/send_private_msg?user_id={0}&message={1}'.format(uid,'已设置为状态 ' + str(statu)))
+    if statu == 1 or statu == 0 :
+        status = statu
+        requests.get(url=uurl+'/send_private_msg?user_id={0}&message={1}'.format(uid,'已设置为状态 ' + str(statu)))
     
-    
+#自动回复    
 def ignore(uid):
     msg = 'Cc不在，我是机器人，你可以更改status码来调教我'
     requests.get(url=uurl+'/send_private_msg?user_id={0}&message={1}'.format(uid, msg))
     
-    
+#自动反馈    
 def feedback(message,uid,gid):
     f = open("feedback.txt","a")
     f.write(message[9: ] + '\n \n')
--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from flask import Flask, request
 
-'''注意，这里的import api是另一个py文件，下文会提及'''
 import api
 
 app = Flask(__name__)
 
 '''监听端口获取QQ信息'''
 @app.route('/', methods=["POST"])
+
 def post_data():
 	'下面的request.get_json().get......是用来获取关键字的值用的，关键字参考上面代码段的数据格式'
+	
+	
 	if request.get_json().get('message_type')=='private': # 如果是私聊信息		
 		uid = request.get_json().get('sender').get('user_id') # 获取信息发送者的QQ号码
 		message = request.get_json().get('raw_message') # 获取原始信息
 		api.keyword_pr(message, uid) # 将 Q号和原始信息传到我们的后台
+		
+		
 	if request.get_json().get('message_type')=='group':# 如果是群聊信息
 		gid = request.get_json().get('group_id') # 获取群号
 		uid = request.get_json().get('sender').get('user_id') # 获取信息发送者的QQ号码
@@ -24,5 +28,5 @@ def post_data():
 
 
 if __name__ == '__main__':
-	app.run(debug=True, host='127.0.0.1', port=12334)# 此处的host和port对应上面 yml文件的设置
+	app.run(debug=True, host='127.0.0.1', port=22334)# 此处的host和port对应 yml文件的设置
 
